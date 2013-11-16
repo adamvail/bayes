ace1b33c8f5ad827907eb2cebe550d43da902122
Adam Vail <adam.r.vail@gmail.com>
2013-11-16 11:26:17 -0600

    starting the testing framework

--- src/naive/NaiveBayes.java
+++ src/naive/NaiveBayes.java
@@ -2,14 +2,20 @@ package naive;
 
 import arff.Data;
 import arff.Feature;
+import arff.Instance;
 
 public class NaiveBayes {
 	
-	Data data;
+	Data train;
+	Data test;
 	NaiveRoot root;
 	
-	public NaiveBayes(Data data){
-		this.data = data;
+	public NaiveBayes(Data train, Data test){
+		this.train = train;
+		this.test = test;
+		
+		createNaiveBayesNetwork();
+		testNetwork();
 	}
 	
 	public void createNaiveBayesNetwork(){
@@ -22,22 +28,33 @@ public class NaiveBayes {
 	 * This is the classification node
 	 */
 	private void createRoot(){
-		Feature classificationFeature = data.getFeatures().get(data.getFeatures().size() - 1);
-		root = new NaiveRoot(classificationFeature, data);
+		Feature classificationFeature = train.getFeatures().get(train.getFeatures().size() - 1);
+		root = new NaiveRoot(classificationFeature, train);
 	}
 	
 	private void createLeaves(){
 		
-		for(Feature f : data.getFeatures()){
+		for(Feature f : train.getFeatures()){
 			// Don't want a leaf that is the same as the root
 			if(f.getName().equals(root.getName())){
 				continue;
 			}
 			
-			NaiveUnit leaf = new NaiveUnit(f, data);
+			NaiveUnit leaf = new NaiveUnit(f, train);
 			leaf.setParent(root);
 			root.addChild(leaf);	
 		}		
 	}
 
+	private void testNetwork(){
+		
+		for(Instance i : test.getData()){
+			
+		}
+	}
+	
+	private String classify(Instance i){
+		
+		return null;
+	}
 }
